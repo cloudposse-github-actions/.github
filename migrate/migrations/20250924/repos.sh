81963d07f12ad9b6d42525d1e08f80b65cc54f70
Igor Rodionov <goruha@gmail.com>
2025-09-24 12:58:07 +0200

    Get repos list

--- migrate/migrations/20250924/repos.sh
+++ migrate/migrations/20250924/repos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gh repo list cloudposse-terraform-components --limit 500 --json name,owner \
-    --jq '.[] | .owner.login + "/" + .name' > repos.txt
+gh repo list cloudposse-github-actions --limit 500 --json name,owner \
+    --jq '.[] | select(.name | test("^.github") | not) | .owner.login + "/" + .name' > repos.txt
 
 split -d -l 16 repos.txt repos-
