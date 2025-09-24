774585afcc3a519fe33849c5fd11b5aba55e28a7
Igor Rodionov <goruha@gmail.com>
2025-09-24 13:59:18 +0200

    Fix repos

--- migrate/migrations/20250924/repos.sh
+++ migrate/migrations/20250924/repos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gh repo list cloudposse-github-actions --limit 500 --json name,owner \
-    --jq '.[] | select(.name | test("^.github") | not) | .owner.login + "/" + .name' > repos.txt
+    --jq '.[] | select(.name | test("^.github") | not) | select(.name | test("^security") | not) | select(.name | test("^test") | not) | .owner.login + "/" + .name' > repos.txt
 
 split -d -l 16 repos.txt repos-
